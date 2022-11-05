7deb6a3ee175e96369b6af9cdb448af4abfdb756
Gagan Bhatia <83947009+GaganBhatia19@users.noreply.github.com>
2022-11-05 21:44:47 +0530

    Time: 1 ms (92.95%), Space: 54.3 MB (13.41%) - LeetHub

--- 0785-is-graph-bipartite/0785-is-graph-bipartite.java
+++ 0785-is-graph-bipartite/0785-is-graph-bipartite.java
@@ -1,36 +1,30 @@
+// dfs approach
 class Solution {
     public boolean isBipartite(int[][] graph) {
         int n = graph.length;
-        ArrayDeque<Integer> queue = new ArrayDeque<>();
         int[]coloured = new int[n];
         Arrays.fill(coloured, -1);
-        for(int gi=0; gi<n; gi++) {
-            queue.offer(gi);
-            coloured[gi] = -2; // marking -2  to say that the particular node has been touched
-            while(!queue.isEmpty()) {
-                int cur = queue.poll();
-                int val = -1;
-                for(int i=0; i<graph[cur].length; i++) {
-                    int ni = graph[cur][i];
-                    if(coloured[ni] != -2){
-                        if(coloured[ni] == -1){ 
-                            queue.offer(ni);
-                            coloured[ni] = -2;
-                        }
-                        else {
-                            if(val == -1) {
-                                if(coloured[ni] == 0) val = 1;
-                                else if(coloured[ni] == 1) val = 0;
-                            } else {
-                                if(val == coloured[ni])
-                                    return false;
-                            }
-                        }
-                    }
-                }
-                coloured[cur] = val==-1?0:val;
+        for(int i = 0; i<n; i++) { // graph may not be connected
+            if(coloured[i] == -1) {
+                coloured[i] = 0;
+                if(!dfs(i, -1, graph, coloured)) return false;
             }
         }
         return true;
+    }
+    private boolean dfs(int curi, int previ, int[][]graph, int[]coloured) {
+        if(previ != -1) {
+            if(coloured[previ] == 0) coloured[curi] = 1;
+            else coloured[curi] = 0;
+        }
+        boolean res = true;
+        for(int vi:graph[curi]) {
+            if(coloured[vi] == -1) res &= dfs(vi, curi, graph, coloured);
+            else {
+                if(coloured[vi] == coloured[curi]) 
+                    return false;
+            }
+        }
+        return res;
     }
 }
